520dc35c86ad906666ea47bba2b3058841ed7aff
huzhenjie <huzhenjie.dev@gmail.com>
2020-01-19 12:56:26 +0800

    feedback list

--- controllers/common.js
+++ controllers/common.js
@@ -7,6 +7,24 @@ module.exports = {
             app, pt, ch, ver, uid, contract, content,
             create_time: new Date().getTime()
         });
-        res.send({code: 200, msg: 'ok'})
+        res.send({code: 200})
+    },
+    getFeedbackList: function (req, res) {
+        let {index, size} = req.query;
+        if (!index) {
+            index = 0
+        }
+        if (!size || size <= 0 || size > 60) {
+            size = 60;
+        }
+        Feedback.findAll({
+            offset: parseInt(index),
+            limit: parseInt(size),
+            order: [
+                ['create_time', 'DESC']
+            ]
+        }).then(function (feedbacks) {
+            res.send({code: 200, data: feedbacks});
+        });
     }
 };
--- routes/common.js
+++ routes/common.js
@@ -6,5 +6,6 @@ const router = express.Router();
 const CommonController = require('../controllers/common');
 
 router.post('/feedback', CommonController.addFeedback);
+router.get('/feedback', CommonController.getFeedbackList);
 
 module.exports = router;
